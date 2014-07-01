26fc481ae716f4387dabcae0031bf00a03a4d558
Léo-Paul COUTURIER <leopaul3@gmail.com>
2014-07-01 17:23:15 +0200

    Init

---
+++ index.php
@@ -0,0 +1,94 @@
+<?php require_once( 'AppInfo.php'); if (substr(AppInfo::getUrl(), 0, 8) !='https://' && $_SERVER[ 'REMOTE_ADDR'] !='127.0.0.1' ) { header( 'Location: https://'. $_SERVER[ 'HTTP_HOST'] . $_SERVER[ 'REQUEST_URI']); exit(); } ?>
+<!DOCTYPE html>
+<html xmlns:fb="http://ogp.me/ns/fb#" lang="en">
+
+<head>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=2.0, user-scalable=yes" />
+	<title>HexaBump</title>
+	<link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.css" />
+	<link rel="stylesheet" href="styles/main.css">
+	<meta property="og:title" content="HexaBump" />
+	<meta property="og:type" content="game" />
+	<meta property="og:url" content="<?php echo AppInfo::getUrl(); ?>" />
+	<meta property="og:image" content="<?php echo AppInfo::getUrl('/logo.png'); ?>" />
+	<meta property="og:description" content="Brick breaker with 6 faces" />
+	<meta property="fb:app_id" content="<?php echo AppInfo::appID(); ?>" />
+</head>
+
+<body>
+	<div id="fb-root"></div>
+	<script type="text/javascript">
+		window.fbAsyncInit = function() {
+			FB.init({
+				appId: '<?php echo AppInfo::appID(); ?>',
+				channelUrl: '//<?php echo $_SERVER["HTTP_HOST"]; ?>/channel.html',
+				status: true,
+				cookie: true,
+				xfbml: true
+			});
+			FB.Event.subscribe('auth.login', function(response) {
+				window.location = window.location;
+			});
+
+			FB.Canvas.setAutoGrow();
+		};
+
+		(function(d, s, id) {
+			var js, fjs = d.getElementsByTagName(s)[0];
+			if (d.getElementById(id)) return;
+			js = d.createElement(s);
+			js.id = id;
+			js.src = "//connect.facebook.net/en_US/all.js";
+			fjs.parentNode.insertBefore(js, fjs);
+		}(document, 'script', 'facebook-jssdk'));
+	</script>
+
+	<div class="container">
+		<h1 class="row">hexabump</h1>
+		<div id="result">
+			<div>
+				<span></span>
+				<div class="btn-group">
+					<button id="share" type="button" class="btn btn-default">Share</button>
+					<button id="restart" type="button" class="btn btn-default">Try again</button>
+				</div>
+			</div>
+		</div>
+	</div>
+	<script>
+		(function(b, o, i, l, e, r) {
+			b.GoogleAnalyticsObject = l;
+			b[l] || (b[l] =
+				function() {
+					(b[l].q = b[l].q || []).push(arguments)
+				});
+			b[l].l = +new Date;
+			e = o.createElement(i);
+			r = o.getElementsByTagName(i)[0];
+			e.src = '//www.google-analytics.com/analytics.js';
+			r.parentNode.insertBefore(e, r)
+		}(window, document, 'script', 'ga'));
+		ga('create', 'UA-9086978-14');
+		ga('send', 'pageview');
+	</script>
+
+	<script src="bower_components/jquery/dist/jquery.js"></script>
+	<script src="bower_components/bootstrap/dist/js/bootstrap.js"></script>
+	<script src="bower_components/physicsjs/dist/physicsjs-full-0.6.0.js"></script>
+	<script src="bower_components/bootstrap/js/affix.js"></script>
+	<script src="bower_components/bootstrap/js/alert.js"></script>
+	<script src="bower_components/bootstrap/js/dropdown.js"></script>
+	<script src="bower_components/bootstrap/js/tooltip.js"></script>
+	<script src="bower_components/bootstrap/js/modal.js"></script>
+	<script src="bower_components/bootstrap/js/transition.js"></script>
+	<script src="bower_components/bootstrap/js/button.js"></script>
+	<script src="bower_components/bootstrap/js/popover.js"></script>
+	<script src="bower_components/bootstrap/js/carousel.js"></script>
+	<script src="bower_components/bootstrap/js/scrollspy.js"></script>
+	<script src="bower_components/bootstrap/js/collapse.js"></script>
+	<script src="bower_components/bootstrap/js/tab.js"></script>
+	<script src="scripts/main.js"></script>
+</body>
+
+</html>
---
+++ scripts/main.js
@@ -0,0 +1,296 @@
+function initGame() {
+	Physics(function (world) {
+		var viewWidth = 1170;
+		var viewHeight = 500;
+
+		window.renderer = Physics.renderer('canvas', {
+			el: document.querySelector('.container'),
+			width: viewWidth,
+			height: viewHeight,
+			meta: false,
+			styles: {
+				'circle': {
+					lineWidth: 0,
+					fillStyle: '#718a01',
+					angleIndicator: '#718a01'
+				},
+				'convex-polygon': {
+					strokeStyle: '#008fd2',
+					lineWidth: 3,
+					fillStyle: '#282828',
+					angleIndicator: '#282828'
+				}
+			}
+		});
+		renderer.el.classList.add('row');
+		world.add(renderer);
+
+		world.on('step', function () {
+			world.render();
+		});
+
+		var viewportBounds = Physics.aabb(0, 0, viewWidth, viewHeight);
+		world.add(Physics.behavior('edge-collision-detection', {
+			aabb: viewportBounds,
+			restitution: 1,
+			cof: 1
+		}));
+
+		window.ball = Physics.body('circle', {
+			x: 50,
+			y: 30,
+			vx: 0.1,
+			vy: 0.1,
+			radius: 20,
+			restitution: 1,
+			cof: 1
+		});
+		world.add(ball);
+
+		for (var i = 33; i + 30 < viewWidth; i += 45) {
+			var j;
+
+			if ((i - 33) % 90 == 0) {
+				j = 28;
+			} else {
+				j = 53;
+			}
+
+			for (; j + 25 < viewHeight; j += 50) {
+				if (Math.random() < 0.4) {
+					spawnBrickAt(i, j);
+				}
+			}
+		}
+
+		world.add(Physics.behavior('sweep-prune'));
+		world.add(Physics.behavior('body-collision-detection'));
+		world.add(Physics.behavior('body-impulse-response'));
+
+		var score = 0;
+		world.on('collisions:detected', function (data) {
+			var c, radius;
+			for (var i = 0, l = data.collisions.length; i < l; i++) {
+				c = data.collisions[i];
+				radius = ball.radius;
+				if (c.bodyA.state.objType == 'brick') {
+					world.remove(c.bodyA);
+					radius++;
+					score++;
+				} else if (c.bodyB.state.objType == 'brick') {
+					world.remove(c.bodyB);
+					radius++;
+					score++;
+				}
+
+				updateRadius(radius);
+			}
+		});
+
+		function onKeydown(e) {
+			var force = 0.05,
+				radius = ball.radius;
+			radius--;
+			switch (e.keyCode) {
+			case 37: // Left
+				ball.applyForce({
+					x: -force,
+					y: 0
+				});
+				break;
+			case 38: // Up
+				ball.applyForce({
+					x: 0,
+					y: -force
+				});
+				break;
+			case 39: // Right
+				ball.applyForce({
+					x: force,
+					y: 0
+				});
+				break;
+			case 40: // Down
+				ball.applyForce({
+					x: 0,
+					y: force
+				});
+				break;
+			default:
+				radius++;
+				break;
+			}
+			updateRadius(radius);
+		}
+
+		document.addEventListener('keydown', onKeydown, false);
+
+		function updateRadius(radius) {
+			if (radius > 0) {
+				if (radius < 10)
+					renderer.options.styles.circle.fillStyle = renderer.options.styles.circle.angleIndicator = '#a42a3c';
+				else
+					renderer.options.styles.circle.fillStyle = renderer.options.styles.circle.angleIndicator = '#718a01';
+
+				ball.geometry.radius = ball.radius = radius;
+				ball.view = renderer.createView(ball.geometry, renderer.options.styles.circle);
+				ball.recalc();
+			} else
+				endGame();
+		}
+
+		var frame = 0,
+			deflateInterval = 50,
+			spawnInterval = 60 * 4;
+		world.on('step', function (data) {
+			frame++;
+			if (frame % deflateInterval == 0)
+				updateRadius(ball.radius - 1);
+			if (frame % spawnInterval == 0)
+				spawnBrick();
+		});
+
+		function buildQuery(x, y) {
+			return Physics.query({
+				name: 'convex-polygon',
+				x: x,
+				y: y,
+			});
+		}
+
+		function spawnBrickAt(x, y) {
+			var brick = Physics.body('convex-polygon', {
+				treatment: 'static',
+				x: x,
+				y: y,
+				restitution: 1,
+				cof: 1,
+				vertices: [
+					{
+						x: -15,
+						y: -25
+				},
+					{
+						x: 15,
+						y: -25
+				},
+					{
+						x: 30,
+						y: 0
+				},
+					{
+						x: 15,
+						y: 25
+				},
+					{
+						x: -15,
+						y: 25
+				},
+					{
+						x: -30,
+						y: 0
+				}
+			]
+			});
+			brick.state.objType = 'brick';
+			world.add(brick);
+		}
+
+		function spawnBrick() {
+			var spawned = false;
+			while (!spawned) {
+				var x = (Math.ceil(Math.random() * ((viewWidth / 45) - 1)) * 45) + 33,
+					y = (Math.ceil(Math.random() * ((viewHeight / 50) - 1)) * 50);
+				if ((x - 33) % 90 == 0) {
+					y += 28;
+				} else {
+					y += 53;
+				}
+				var query = world.find(buildQuery(x, y));
+				if (query.length == 0) {
+					spawnBrickAt(x, y);
+					spawned = true;
+				}
+			}
+		}
+
+		var touches = {};
+		document.addEventListener('touchmove', function (e) {
+			for (var i in e.touches) {
+				var touch = e.touches[i];
+				if (touch.pageX && touch.pageY) {
+					if (touches[touch.identifier]) {
+						touches[touch.identifier].dx = touch.pageX - touches[touch.identifier].x;
+						touches[touch.identifier].dy = touch.pageY - touches[touch.identifier].y;
+						touches[touch.identifier].x = touch.pageX;
+						touches[touch.identifier].y = touch.pageY;
+					} else {
+						touches[touch.identifier] = {
+							x: touch.pageX,
+							y: touch.pageY
+						};
+					}
+				}
+			}
+		}, false);
+
+		document.addEventListener('touchend', function (e) {
+			for (var i in e.changedTouches) {
+				var touch = e.changedTouches[i];
+				if (touches[touch.identifier]) {
+					ball.applyForce({
+						x: Number(touches[touch.identifier].dx > 0) * 0.01,
+						y: Number(touches[touch.identifier].dy > 0) * 0.01
+					});
+					updateRadius(ball.radius - 1);
+					console.log(touches[touch.identifier]);
+				}
+			}
+		}, false);
+
+		var startTime = performance.now();
+
+		function fbShare(time) {
+			return function (e) {
+				FB.ui({
+					method: "share_open_graph",
+					action_type: "hexabump:score",
+					action_properties: JSON.stringify({
+						game: "http://hexabump.herokuapp.com/",
+						time: time.toISOString()
+					})
+				}, function (response) {
+					console.log(response);
+				});
+			};
+		}
+
+		function endGame() {
+			if (Physics.util.ticker.isActive()) {
+				var time = performance.now() - startTime,
+					date = new Date(time);
+				document.removeEventListener('keydown', onKeydown);
+				Physics.util.ticker.stop();
+				document.querySelector("#result").querySelector("span").innerHTML = "Time: " + date.getMinutes() + ":" + date.getSeconds() + "." + date.getMilliseconds();
+				document.querySelector(".container").classList.add("end");
+				document.querySelector("#share").onclick = fbShare(date);
+			}
+		}
+
+		Physics.util.ticker.on(function (time, dt) {
+			world.step(time);
+		});
+
+		Physics.util.ticker.start();
+	});
+}
+
+function restartGame() {
+	document.querySelector("canvas").remove();
+	document.querySelector(".container").classList.remove('end');
+	initGame();
+}
+
+document.querySelector("#restart").addEventListener('click', restartGame, false);
+
+initGame();
