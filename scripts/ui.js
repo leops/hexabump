83133a2ce9c4342e1681e2d9bfb467ce76f77d4c
Léo-Paul COUTURIER <leopaul3@gmail.com>
2014-07-19 11:18:26 +0200

    Destroy highscore tooltip when leaving score screen

--- scripts/ui.js
+++ scripts/ui.js
@@ -11,32 +11,35 @@ function timeFormat(time) {
 
 function title() {
 	$('.container').addClass('show').find('#ui').attr('class', 'title');
-	FB.api('/me/scores', 'GET', function(response){
+	FB.api('/me/scores', 'GET', function (response) {
 		$('#title').find('span').html(timeFormat(response.data[0].score));
 	});
 }
 
 function result(time) {
-	if(time > 60000)
+	if (time > 60000)
 		publishAchievement(1);
 	$('#shareScore').removeClass('btn-success').removeClass('btn-danger');
 	$("#result span").html(timeFormat(time));
 	$('.container').addClass('show').find('#ui').attr('class', 'result');
-	$('#shareScore').unbind('click').click(function(e) {
-		shareScore(Math.round(time), function(res) {
+	$('#shareScore').unbind('click').click(function (e) {
+		shareScore(Math.round(time), function (res) {
 			console.log('shareScore', res);
-			if(res && !res.error)
+			if (res && !res.error)
 				$('#shareScore').removeClass('btn-danger').addClass('btn-success');
 			else
 				$('#shareScore').addClass('btn-danger').removeClass('btn-success');
 		});
 	});
-	FB.api('/me/score', function(response) {
-		if(response.data && response.data[0] && response.data[0].score < time) {
+	FB.api('/me/score', function (response) {
+		if (response.data && response.data[0] && response.data[0].score < time) {
 			$("#result span").tooltip({
 				title: 'New highscore!',
 				trigger: 'manual'
 			}).tooltip('show');
+			$('#restart, .titleBtn').one(function () {
+				$("#result span").tooltip('destroy');
+			})
 			sendScore(time, function (response) {
 				if (response.error) {
 					console.error('sendScore failed', response);
@@ -52,9 +55,9 @@ function result(time) {
 
 function highscores() {
 	$('.container').addClass('show').find('#ui').attr('class', 'highscore');
-	getHighscores(function(res) {
+	getHighscores(function (res) {
 		$('#highscore tbody').html('');
-		res.data.forEach(function(data, index) {
+		res.data.forEach(function (data, index) {
 			$('#highscore tbody').append($('<tr><td>' + (index + 1) + '</td><td><img src="' + data.user.picture.data.url + '" alt="Picture"/></td><td>' + data.user.name + '</td><td>' + timeFormat(data.score) + '</td></tr>'));
 		});
 	});
@@ -62,14 +65,14 @@ function highscores() {
 
 function achievements() {
 	$('.container').addClass('show').find('#ui').attr('class', 'achievements');
-	getAchievements(function(res) {
+	getAchievements(function (res) {
 		$('#achievements tbody').html('');
-		res.data.forEach(function(achieves) {
+		res.data.forEach(function (achieves) {
 			var achievement = achieves.data.achievement,
 				xhr = new XMLHttpRequest()
-			xhr.open('GET', achievement.url.replace('http:', 'https:'), true);
-			xhr.onreadystatechange = function(e) {
-				if(xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200 && xhr.responseXML) {
+				xhr.open('GET', achievement.url.replace('http:', 'https:'), true);
+			xhr.onreadystatechange = function (e) {
+				if (xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200 && xhr.responseXML) {
 					$('#achievements tbody').append($('<tr><td><img height="56px" width="56px" src="' + $(xhr.responseXML).find("meta[property='og:image']").attr('content') + '" alt="Icon"/></td><td>' + $(xhr.responseXML).find("meta[property='og:title']").attr('content') + '</td><td>' + $(xhr.responseXML).find("meta[property='og:description']").attr('content') + '</td><td>' + $(xhr.responseXML).find("meta[property='game:points']").attr('content') + '</td></tr>'));
 				}
 			};
